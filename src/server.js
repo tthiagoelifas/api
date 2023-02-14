fa0dc97f0bdc3ddab28a517411185982f706168e
tthiagoelifas <tthiagoelifas@gmail.com>
2023-02-14 11:48:22 -0300

    creating server routes

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.get("/message/:id", (request, response) => {
+  response.send(`Id da mensagem: ${request.params.id}`)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
