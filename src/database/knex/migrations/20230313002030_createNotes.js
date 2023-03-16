8548f939a5c84375f0d7ef3bb665f4c2d0cf08f9
tthiagoelifas <tthiagoelifas@gmail.com>
2023-03-15 22:22:13 -0300

    create tags and links

--- src/database/knex/migrations/20230313002030_createNotes.js
+++ src/database/knex/migrations/20230313002030_createNotes.js
@@ -1,4 +1,3 @@
-
 exports.up = knex => knex.schema.createTable("notes", table => {
   table.increments("id");
   table.text("title");
@@ -8,6 +7,5 @@ exports.up = knex => knex.schema.createTable("notes", table => {
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
 });
-
 
 exports.down = knex => knex.schema.dropTable("notes");
